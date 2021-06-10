79da3606431bd35b95847a94bec9005697c76b29
alanisaias24147 <78932267+alanisaias24147@users.noreply.github.com>
2021-06-10 02:22:26 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Alumnos;
+
+namespace alumnos
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona("Ernesto", "Ordoñez");
+            Console.WriteLine(persona1.nombreCompleto);
+
+            persona1.apellido = "Linares";
+            Console.WriteLine(persona1.nombreCompleto);
+            persona1.presentarse();
+
+            persona1.ColorFavorito = "morado";
+            Console.WriteLine(persona1.ColorFavorito);
+
+            
+        }
+    }
+}
---
+++ Class2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using alumnos;
+
+namespace Alumnos
+{
+    class Alumno : Persona
+    {
+        public Alumno(string nombre, string apellido) : base(nombre, apellido)
+        {
+
+        }
+    }
+}
